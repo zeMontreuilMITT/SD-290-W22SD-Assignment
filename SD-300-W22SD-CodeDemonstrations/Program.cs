620432b7ca8404e93a8c93d50193af418ec02292
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2022-06-15 13:14:44 -0500

    Add the static School and Enrollment classes

--- SD-300-W22SD-CodeDemonstrations/Program.cs
+++ SD-300-W22SD-CodeDemonstrations/Program.cs
@@ -1,48 +1,113 @@
-﻿Calendar myCalendar = new Calendar();
+﻿School.CreateStudent("Batman");
+Student studentToAdd = School.GetStudentByName("Batman");
 
-Date halloween = new Date("Halloween", 31);
-myCalendar.Dates.Add(halloween);
-Console.WriteLine(myCalendar.Dates.First().Title);
+School.AddCourse("Intro to Batman", 500);
 
-class Calendar
+Course courseToAdd = School.GetCourseByNumber(500);
+School.AddStudentToCourse(studentToAdd, courseToAdd);
+
+class Student
 {
-    public string Name { get;set;}  
-    public int Year { get;set;}
-    public List<Date> Dates { get; set; }
-    public Calendar()
+    public string Name { get; set; }
+    public List<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+    public Student()
     {
-        Name = "Year 2000";
-        Year = 2000;
-        Dates = new List<Date>();
     }
 
-    // overload
-    public Calendar(string name, int year)
+    public Student(string name)
     {
         Name = name;
-        Year = year;
-        Dates = new List<Date>();
     }
 }
-class Date
+
+class Course
 {
-    public string Title { get;set;}
-    private int _dayOfMonth { get;set;}
-    public int DayOfMonth
+    public string Title { get; set; }
+    public int Number { get; set; }
+    public List<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+
+    public Course()
     {
-        get { return _dayOfMonth;}
-        set
-        {
-            if(value > 0 && value <= 31)
-            {
-                _dayOfMonth = value;
-            }
-        }
+
     }
 
-    public Date(string title, int day)
+    public Course(string title, int number)
     {
         Title = title;
-        DayOfMonth = day;
+        Number = number;
+    }
+}
+
+static class School
+{
+    public static string Title { get; set; } = "MITT";
+    public static List<Course> Courses { get; set; } = new List<Course>();
+    public static List<Student> Students { get; set; } = new List<Student>();
+    public static List<Enrollment> Enrollmemts { get; set; } = new List<Enrollment>();
+
+    public static Student CreateStudent(string name)
+    {
+        Student newStudent = new Student(name);
+        Students.Add(newStudent);
+        return newStudent;
+    }
+
+    public static void AddCourse(string title,  int number)
+    {
+        Course newCourse = new Course(title, number);
+        Courses.Add(newCourse);
+    }
+    public static void AddStudentToCourse(Student student, Course course)
+    {
+        Enrollment newEnrollment = new Enrollment(student, course);
+        newEnrollment.EnrollmentId = Enrollmemts.Count + 1;
+
+        student.Enrollments.Add(newEnrollment);
+        course.Enrollments.Add(newEnrollment);
+        Enrollmemts.Add(newEnrollment);
+    }
+
+
+    public static Course GetCourseByNumber(int number)
+    {
+        foreach (Course course in Courses)
+        {
+            if (course.Number == number)
+            {
+                return course;
+            }
+        }
+        
+        return null;
+    }
+
+    public static Student GetStudentByName(string name)
+    {
+        foreach(Student student in Students)
+        {
+            if(string.Equals(student.Name, name))
+            {
+                return student;
+            }
+        }
+
+        return null;
+    }
+}
+
+// Change the system so that instead of adding students to lists of courses and vice-versa, 
+// we use an Enrollment class to handle when students are added
+
+class Enrollment
+{
+    public int EnrollmentId { get; set; }
+    public Student Student { get; set; }
+    public Course Course { get; set; }
+    public int Grade { get; set; }
+
+    public Enrollment(Student student, Course course)
+    {
+        Student = student;
+        Course = course;
     }
 }
