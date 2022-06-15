d834f992db1e270d2b40db762e1bc01a7612bb7e
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2022-06-15 09:27:43 -0500

    Initial Commit

---
+++ SD-300-W22SD-CodeDemonstrations/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SD_300_W22SD_CodeDemonstrations.Classes
+{
+    class Person
+    {
+        private string _firstName { get; set; }
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+    }
+}
---
+++ SD-300-W22SD-CodeDemonstrations/Program.cs
@@ -0,0 +1,48 @@
+﻿Calendar myCalendar = new Calendar();
+
+Date halloween = new Date("Halloween", 31);
+myCalendar.Dates.Add(halloween);
+Console.WriteLine(myCalendar.Dates.First().Title);
+
+class Calendar
+{
+    public string Name { get;set;}  
+    public int Year { get;set;}
+    public List<Date> Dates { get; set; }
+    public Calendar()
+    {
+        Name = "Year 2000";
+        Year = 2000;
+        Dates = new List<Date>();
+    }
+
+    // overload
+    public Calendar(string name, int year)
+    {
+        Name = name;
+        Year = year;
+        Dates = new List<Date>();
+    }
+}
+class Date
+{
+    public string Title { get;set;}
+    private int _dayOfMonth { get;set;}
+    public int DayOfMonth
+    {
+        get { return _dayOfMonth;}
+        set
+        {
+            if(value > 0 && value <= 31)
+            {
+                _dayOfMonth = value;
+            }
+        }
+    }
+
+    public Date(string title, int day)
+    {
+        Title = title;
+        DayOfMonth = day;
+    }
+}
